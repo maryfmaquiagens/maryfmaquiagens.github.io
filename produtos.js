102c86142fb6954fb337f65dd5e065de240a70c4
Victor Nunes <157613717+Victorndias@users.noreply.github.com>
2025-03-10 13:45:01 -0300

    Add files via upload

--- produtos.js
+++ produtos.js
@@ -1,31 +1,36 @@
 const produtos = [
   { 
     nome: "Lenço Umedecido", 
+    precoAntigo: 10, 
     preco: 8, 
     imagem: "lenço.jpeg", 
     descricao: "Lenço umedecido para limpeza facial,\n com ingredientes suaves e ideais\n para pele sensível." 
   },
   { 
     nome: "Esponja para Base", 
-    preco: 4, 
+    precoAntigo: 5, 
+    preco: 4,  
     imagem: "produto1.jpeg", 
     descricao: "Esponja macia para aplicação de base\n líquida, garantindo um acabamento natural." 
   },
   { 
     nome: "Rímel", 
+    precoAntigo: 12, 
     preco: 10, 
     imagem: "produto2.jpeg", 
     descricao: "Máscara de cílios extra volume que realça\n seus olhos com volume e definição." 
   },
   { 
     nome: "Água Micelar", 
-    preco: 12, 
+    precoAntigo: 15, 
+    preco: 12,
     imagem: "aguamicelar.jpeg", 
     descricao: "Água micelar suave que remove impurezas\n e maquiagem sem agredir a pele.",
     disponibilidade: "esgotado"  // Produto esgotado
   },
   { 
     nome: "Batom", 
+    precoAntigo: 18, 
     preco: 15, 
     imagem: "batom.jpeg", 
     descricao: "Batom de longa duração com cores vibrantes.",
@@ -36,18 +41,21 @@ const produtos = [
   },
   { 
     nome: "Sabonete Facil", 
+    precoAntigo: 18, 
     preco: 15, 
     imagem: "sabonete.jpeg", 
     descricao: "Sabonete para limpeza facial, enriquecido\n com vitamina C." 
   },
   { 
     nome: "Sérum Esfoliante", 
+    precoAntigo: 19, 
     preco: 15, 
     imagem: "serumesfoliante.jpeg", 
     descricao: "Sérum antioxidante que ajuda a iluminar e\n proteger a pele dos radicais livres." 
   },
   { 
     nome: "Gloss", 
+    precoAntigo: 18, 
     preco: 14,  
     imagem: "gloss_vermelho.jpeg",
     descricao: "Gloss brilhante que proporciona um toque\n sedoso e um efeito iluminado aos lábios.",
@@ -59,61 +67,71 @@ const produtos = [
   },
   { 
     nome: "Máscara Facial", 
+    precoAntigo: 3.5, 
     preco: 2, 
     imagem: "mascarafacil.jpeg", 
     descricao: "Máscara facial para suavizar e revitalizar a\n pele, ideal para uma rotina de beleza." 
   },
   { 
     nome: "Iluminador", 
+    precoAntigo: 20, 
     preco: 16, 
     imagem: "iluminador.jpeg", 
     descricao: "Destaca pontos do rosto com brilho e efeito glow." 
   },
   { 
     nome: "Lápis de Olho", 
+    precoAntigo: 10, 
     preco: 7, 
     imagem: "lapis.jpeg", 
     descricao: "Lápis de olho de alta definição para realçar e\n contornar os olhos com precisão." 
   },
   { 
     nome: "Delineador", 
+    precoAntigo: 14, 
     preco: 10, 
     imagem: "delineador.jpeg", 
     descricao: "Delineador prático e preciso para um look\n marcante e bem definido." 
   },
   { 
     nome: "Escova de Silicone", 
+    precoAntigo: 16, 
     preco: 12, 
     imagem: "escovasilicone.jpeg", 
     descricao: "Massageia o couro cabeludo, ajudando na\n limpeza e estimulação capilar." 
   },
   { 
     nome: "Esponja para Pó", 
+    precoAntigo: 5, 
     preco: 4, 
     imagem: "esponjapo.jpeg", 
     descricao: "Aplica pó compacto ou solto para um\n acabamento uniforme." 
   },
   { 
     nome: "Apontador", 
-    preco: 10, 
+    precoAntigo: 13, 
+    preco: 10,
     imagem: "apontador.jpeg", 
     descricao: "Mantém lápis de olho e boca afiados\n para maior precisão.",
     disponibilidade: "esgotado" 
   },
   { 
     nome: "Blindagem", 
-    preco: 14, 
+    precoAntigo: 18, 
+    preco: 14,
     imagem: "blindagem.jpeg", 
     descricao: "Protege e fixa a maquiagem, aumentando\n sua durabilidade." 
   },
   { 
     nome: "Paleta de Sombra", 
+    precoAntigo: 24, 
     preco: 20, 
     imagem: "paletasombra.png", 
     descricao: "Define e colore as pálpebras,\n realçando o olhar." 
   },
   { 
     nome: "Primer Facil", 
+    precoAntigo: 20, 
     preco: 16, 
     imagem: "primerfacial.jpeg", 
     descricao: "Prepara a pele, suavizando poros e melhorando\n a fixação da maquiagem.",
@@ -121,12 +139,14 @@ const produtos = [
   },
   { 
     nome: "Sérum Clareador", 
+    precoAntigo: 19, 
     preco: 14, 
     imagem: "serumclareador.jpeg", 
     descricao: "Uniformiza o tom da pele e ajuda\n a reduzir manchas." 
   },
   { 
     nome: "Lip Gloss", 
+    precoAntigo: 18, 
     preco: 15, 
     imagem: "lipgloss.jpeg", 
     descricao: "Dá brilho e efeito molhado aos lábios.",
@@ -134,31 +154,36 @@ const produtos = [
   },
   { 
     nome: "Cílios", 
+    precoAntigo: 12, 
     preco: 10, 
     imagem: "cilios.jpeg", 
     descricao: "Alongam e dão volume aos olhos,\n destacando o olhar." 
   },
   { 
     nome: "Sérum Rosa Mosqueta", 
-    preco: 15, 
+    precoAntigo: 20, 
+    preco: 15,
     imagem: "serumrosa.jpeg", 
     descricao: "Hidrata e auxilia na regeneração\n da pele." 
   },
   { 
-    nome: "Spray Fixado de Maquiagens", 
+    nome: "Spray Fixador de Maquiagens", 
+    precoAntigo: 18, 
     preco: 15, 
     imagem: "spray.jpeg", 
     descricao: "O Fix Make UP foi criado para aumentar a\n durabilidade da maquiagem de forma leve e bem prática,\n ele possui secagem rápida fazendo com que\n a pele não fique úmida." 
   },
   { 
     nome: "Mousse Micelar", 
+    precoAntigo: 24, 
     preco: 16,  
     imagem: "mousse.jpeg", 
     descricao: "Vegano não ressega a pele,\n HIDRATAÇÃO EQUILIBRADA,\n CONTROLA A OLEOSIDADE,\n ESTIMULA A RENOVAÇÃO CELULAR,\n DERMATOLOGICAMENTE TESTADO.",
     disponibilidade: "esgotado" 
   },
   { 
-    nome: "Black mask com colageno", 
+    nome: "Black Mask com Colageno", 
+    precoAntigo: 20, 
     preco: 14, 
     imagem: "blackmask.jpeg", 
     descricao: "Remove cravos e espinhas\n controle de oleosidade\n melhora a aparência da pele.", 
@@ -166,19 +191,22 @@ const produtos = [
   },
   { 
     nome: "Esfoliante Corporal", 
-    preco: 15, 
+    precoAntigo: 20, 
+    preco: 15,      
     imagem: "esfoliantecorporal.jpeg", 
     descricao: "Esfoliação eficiente e muito agradável\n sem agredir a pele, removendo células mortas,\n eliminando acúmulo de sujeiras e produtos depositados nos poros,\n revelando a uniformidade do tom da pele,\n toque ultra macio, textura lisa e sedosa.", 
-    disponibilidade: "esgotado" 
-  },
+    disponibilidade: "esgotado"
+  },  
   { 
     nome: "Torre de Pigmento Neon", 
-    preco: 12, 
+    precoAntigo: 14, 
+    preco: 10, 
     imagem: "torre.jpeg", 
     descricao: "Conjunto de sombras vibrantes e bem pigmentadas,\n ideal para maquiagens artísticas e looks de\n Carnaval, trazendo cores intensas e chamativas." 
   },
   { 
     nome: "Batom Magic", 
+    precoAntigo: 5, 
     preco: 4, 
     imagem: "vermelhomagic.jpeg", 
     descricao: "Batom de longa duração com cores vibrantes.",
@@ -186,7 +214,15 @@ const produtos = [
       { cor: "Vermelho", imagem: "vermelhomagic.jpeg" },
       { cor: "Roxo", imagem: "roxomagic.jpeg" }
     ]
-  }
+  },
+  { 
+    nome: "Pó Facial Translúcido", 
+    precoAntigo: 14, 
+    preco: 10, 
+    imagem: "potranslucido.jpeg", 
+    descricao: "O pó facial translúcido serve para fixar\n a maquiagem, matificar a pele e controlar a\n oleosidade. Ele é incolor e tem uma textura leve e ultrafina.",
+    disponibilidade: "esgotado"
+  },
 ];
 
 // Número inicial de produtos a serem exibidos
@@ -237,6 +273,20 @@ function carregarProdutos(termoBusca = "", mostrarTodos = false) {
       `;
     }
 
+    // Se o produto possui precoAntigo, exibe-o com linha cortada e o preço atual em destaque;
+    // caso contrário, exibe apenas o preço atual.
+    let precoHTML = "";
+    if (produto.precoAntigo) {
+      precoHTML = `
+        <div class="flex flex-col items-center mt-1">
+          <span class="text-gray-500 text-sm line-through">R$ ${produto.precoAntigo.toFixed(2)}</span>
+          <span class="text-lg font-bold text-black-600">R$ ${produto.preco.toFixed(2)}</span>
+        </div>
+      `;
+    } else {
+      precoHTML = `<p class="text-lg font-bold mt-1 text-center">R$ ${produto.preco.toFixed(2)}</p>`;
+    }
+
     const produtoHTML = `
       <div class="relative bg-[#ccac8e] p-4 rounded-lg shadow hover:scale-105 transition-transform">
         <!-- Container da imagem com overlay se esgotado -->
@@ -245,7 +295,7 @@ function carregarProdutos(termoBusca = "", mostrarTodos = false) {
           ${esgotadoHTML}
         </div>
         <h2 class="text-lg font-bold mt-1 text-center">${produto.nome}</h2>
-        <p class="text-lg font-bold mt-1 text-center">R$ ${produto.preco.toFixed(2)}</p>
+        ${precoHTML}
         
         <!-- Botão de Descrição: sempre exibido -->
         <div class="w-full flex justify-center mb-2">
@@ -285,7 +335,7 @@ function carregarProdutos(termoBusca = "", mostrarTodos = false) {
 }
 
 function mostrarMaisProdutos() {
-  // Incrementa o número de produtos exibidos em 8 a cada clique
+  // Incrementa o número de produtos exibidos em 11 a cada clique
   produtosExibidos += 11;
   carregarProdutos();
 }
